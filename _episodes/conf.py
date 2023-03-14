8af76d38d13c2fd80a61794e77b0edf5b4682801
Jessica Nash <janash@vt.edu>
2023-03-14 12:00:23 -0400

    update github URL

--- _episodes/conf.py
+++ _episodes/conf.py
@@ -112,7 +112,7 @@ html_favicon = "_static/molssi_square.png"
 # documentation.
 #
 html_theme_options = {
-	"github_url": "https://github.com/jchen0506/molssi_doc_theme",
+	"github_url": "https://github.com/MolSSI-Education/python-package-best-practices",
 	"twitter_url": "https://twitter.com/MolSSI_NSF",
 
 	"logo": {
