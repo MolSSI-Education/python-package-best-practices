9db04c12d9725c5df662cecf17c70f4c002aa45a
Jessica Nash <janash@vt.edu>
2023-03-19 22:02:43 -0400

    fix google analytics formatting

--- _episodes/conf.py
+++ _episodes/conf.py
@@ -123,7 +123,6 @@ html_theme_options = {
       "molssi_dark": "molssi_main_logo_inverted_white.png",
     },
 	"show_toc_level": 2,
-    "google_analytics_id": "G-FHKVGE8HKZ",
 	"header_links_before_dropdown": 4,
 	"external_links": [
       {"name": "MolSSI", "url": "https://molssi.org"}
@@ -133,6 +132,10 @@ html_theme_options = {
     "footer_start": [ "molssi_footer" ],
     "footer_end": [],
     "icon_links":[],
+}
+
+html_theme_options["analytics"] = {
+    "google_analytics_id": "G-FHKVGE8HKZ",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
